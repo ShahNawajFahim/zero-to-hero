e80ac7046e74517ebedfc089eb0d2dd3d6d94864
Shah Nawaj Fahim <shahnawasfahim@gmail.com>
2022-10-12 05:22:59 +0600

    nav

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/blog'>Blogs</Link>
-            <Link to='/statistic'>Statistics</Link>
+        <nav className='nav-bar'>
+            <div><h2>Zero to Hero</h2></div>
+            <div>
+                <Link to='/'>Home</Link>
+                <Link to='/blog'>Blogs</Link>
+                <Link to='/statistic'>Statistics</Link>
+            </div>
         </nav>
     );
 };
